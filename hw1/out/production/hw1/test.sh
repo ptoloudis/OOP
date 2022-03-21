02bcfc349abb717b0e254feefde904a9b2109d60
panagiotis toloudis <57294317+ptoloudis@users.noreply.github.com>
2022-03-21 11:35:35 +0200

    final

---
+++ hw1/out/production/hw1/test.sh
@@ -0,0 +1,5 @@
+for i in {1..30}
+do
+   echo "\n NEW MAKE: $i"
+   make
+done
--- hw1/src/ce326/hw1/HW1.java
+++ hw1/src/ce326/hw1/HW1.java
@@ -5,46 +5,46 @@ import java.util.Scanner;
 
 public class HW1 {
     public static void main(String[] args) {
-        Trie tmp = new Trie();
-        boolean flag;
-        Scanner sc = new Scanner(System.in);
+        Trie tmp = new Trie(); // Create a new Trie
+        boolean flag; // flag for checking if the word is in the dictionary
+        Scanner sc = new Scanner(System.in); // Create a scanner for user input
         String input,word;
         int size;
 
         while (true) {
-            System.out.println("?: ");
+            System.out.println("?: "); // Prompt user for input
 
-            input = sc.next();
-            switch (input) {
-                case "-i" -> {
+            input = sc.next(); // Get user input
+            switch (input) { // Check user input
+                case "-i" -> { // Insert a word into the Trie
                     word = sc.next();
                     tmp.insert(word);
                 }
-                case "-r" -> {
+                case "-r" -> { // Remove a word from the Trie
                     word = sc.next();
                     tmp.delete(word);
                 }
-                case "-f" -> {
+                case "-f" -> { // Find a word in the Trie
                     word = sc.next();
                     flag = tmp.search(word);
-                    if (flag) {
+                    if (flag) { // If the word is in the dictionary
                         System.out.println("FND "+word.toLowerCase()+" OK");
-                    } else {
+                    } else { // If the word is not in the dictionary
                         System.out.println("FND "+word.toLowerCase()+" NOK");
                     }
                 }
-                case "-p" -> tmp.print_preOrder();
-                case "-d" -> tmp.print_dictionary();
-                case "-w" -> {
+                case "-p" -> tmp.print_preOrder(); // Print the Trie in pre-order
+                case "-d" -> tmp.print_dictionary(); // Print the dictionary
+                case "-w" -> { // Print the words same length and different in n characters
                     word = sc.next();
                     size = sc.nextInt();
                     tmp.distant(word, size);
                 }
-                case "-s" -> {
+                case "-s" -> { // Print the words in the dictionary with a given prefix
                     word = sc.next();
                     tmp.suffix_all(word);
                 }
-                case "-t", "-q" -> {
+                case "-q" -> { // Quit the program
                     System.out.println("Bye bye!");
                     System.exit(0);
                 }
